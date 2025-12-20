8de2dca549f49879fb104be3dbdad1e5a46eaebe
Joris <151638489+JorisMentink@users.noreply.github.com>
2025-12-20 05:40:42 +0100

    Repo pruning + enviornment updates

--- mse-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-python train.py -s data/compiled_training_data/Case1_4DCT --iterations 30000 --model_path output/300Projections_Case1_B_2_MSE
-python train.py -s data/compiled_training_data/Case2_4DCT --iterations 30000 --model_path output/300Projections_Case2_B_2_MSE
-python train.py -s data/compiled_training_data/Case3_4DCT --iterations 30000 --model_path output/300Projections_Case3_B_2_MSE
-python train.py -s data/compiled_training_data/Case4_4DCT --iterations 30000 --model_path output/300Projections_Case4_B_2_MSE
-python train.py -s data/compiled_training_data/Case5_4DCT --iterations 30000 --model_path output/300Projections_Case5_B_2_MSE
--- test.py
+++ test.py
@@ -22,6 +22,7 @@ import yaml
 from argparse import ArgumentParser
 from random import randint
 import SimpleITK as sitk
+import nibabel as nib
 
 sys.path.append("./")
 from r2_gaussian.arguments import (
@@ -35,7 +36,6 @@ from r2_gaussian.gaussian import GaussianModel, render, query, initialize_gaussi
 from r2_gaussian.utils.general_utils import safe_state, t2a
 from r2_gaussian.utils.image_utils import metric_vol, metric_proj
 from Hexplane.deformation import deform_network
-import nibabel as nib
 
 
 
@@ -47,8 +47,8 @@ def testing(
     skip_render_train: bool,
     skip_render_test: bool,
     skip_recon: bool,
-    skip_4dct: bool,
 ):
+    
     # Set up dataset
     scene = Scene(
         dataset,
@@ -64,17 +64,11 @@ def testing(
     deform_net = deform_network(deform_params).cuda()
     deform_net_checkpoint_path = os.path.join(dataset.model_path, "ckpt", f"chkpnt{iteration}.pth")
     checkpoint = torch.load(deform_net_checkpoint_path, weights_only=False)
-    for name, param in deform_net.named_parameters():
-        print(f"{name} before loading: {param.data}")
     deform_net.load_state_dict(checkpoint["deformation_net_state_dict"])
-    for name, param in deform_net.named_parameters():
-        print(f"{name} after loading: {param.data}")
     deform_net.eval()
-    print(f"Checkpoint path: {deform_net_checkpoint_path}")
-    print(f"Deformation network architecture: {deform_net}")
-    print(f"Checkpoint keys: {checkpoint.keys()}")
-    print(f"Checkpoint deformation_net_state_dict keys: {checkpoint['deformation_net_state_dict'].keys()}")
-    print(f"Model state_dict keys: {deform_net.state_dict().keys()}")
+
+    #construct 4D ground truth volume
+    gt_vols_4d = scene.info.gt_vols
 
     save_path = osp.join(
         dataset.model_path,
@@ -110,19 +104,8 @@ def testing(
             scene.scanner_cfg,
             gaussians,
             pipeline,
-            scene.vol_gt,
+            gt_vols_4d,
             deform_net,
-        )
-
-    if not skip_4dct:
-        generate_4dct(
-            save_path,
-            "4dct",
-            scene.scanner_cfg,
-            gaussians,
-            pipeline,
-            deform_net,
-            queried_timepoints=list(range(0, 10, 1))
         )
 
 
@@ -132,78 +115,17 @@ def evaluate_volume(
     scanner_cfg,
     gaussians: GaussianModel,
     pipeline: PipelineParams,
-    vol_gt,
-):
-    """Evaluate volume reconstruction."""
-    slice_save_path = osp.join(save_path, name)
-    os.makedirs(slice_save_path, exist_ok=True)
-
-    query_pkg = query(
-        gaussians,
-        scanner_cfg["offOrigin"],
-        scanner_cfg["nVoxel"],
-        scanner_cfg["sVoxel"],
-        pipeline,
-    )
-    vol_pred = query_pkg["vol"]
-
-    psnr_3d, _ = metric_vol(vol_gt, vol_pred, "psnr")
-    ssim_3d, ssim_3d_axis = metric_vol(vol_gt, vol_pred, "ssim")
-
-    multithread_write(
-        [vol_gt[..., i][None] for i in range(vol_gt.shape[2])],
-        slice_save_path,
-        "_gt",
-    )
-    multithread_write(
-        [vol_pred[..., i][None] for i in range(vol_pred.shape[2])],
-        slice_save_path,
-        "_pred",
-    )
-    eval_dict = {
-        "psnr_3d": psnr_3d,
-        "ssim_3d": ssim_3d,
-        "ssim_3d_x": ssim_3d_axis[0],
-        "ssim_3d_y": ssim_3d_axis[1],
-        "ssim_3d_z": ssim_3d_axis[2],
-    }
-
-    with open(osp.join(save_path, "eval3d.yml"), "w") as f:
-        yaml.dump(eval_dict, f, default_flow_style=False, sort_keys=False)
-
-    np.save(osp.join(save_path, "vol_gt.npy"), t2a(vol_gt))
-    np.save(osp.join(save_path, "vol_pred.npy"), t2a(vol_pred))
-    # For visualization with 3D slicer
-    sitk.WriteImage(
-        sitk.GetImageFromArray(t2a(vol_gt).transpose(2, 0, 1)),
-        os.path.join(save_path, "vol_gt.nii.gz"),
-    )
-    sitk.WriteImage(
-        sitk.GetImageFromArray(t2a(vol_pred).transpose(2, 0, 1)),
-        os.path.join(save_path, "vol_pred.nii.gz"),
-    )
-
-    print(f"{name} complete. psnr_3d: {psnr_3d}, ssim_3d: {ssim_3d}")
-
-
-def generate_4dct(
-    save_path,
-    name,
-    scanner_cfg,
-    gaussians: GaussianModel,
-    pipeline: PipelineParams,
+    gt_vols,
     deform_net,
-    queried_timepoints=list(range(0, 10, 1))
 ):
     """Evaluate volume reconstruction."""
     slice_save_path = osp.join(save_path, name)
     os.makedirs(slice_save_path, exist_ok=True)
 
-    queried_volumes = []
-
-    niftii_savepath = osp.join(save_path, f"{name}_4d.nii.gz")
-
-    for timepoint in queried_timepoints:
+    #Load predicted volumes and stack into 4D array
+    vol_pred_list = []
+    for phasebin in range(len(gt_vols)):
+        print(f"Querying volume for timepoint {phasebin}...")
         query_pkg = query(
             gaussians,
             scanner_cfg["offOrigin"],
@@ -211,17 +133,87 @@ def generate_4dct(
             scanner_cfg["sVoxel"],
             pipeline,
             deformation_net=deform_net,
-            time_value=timepoint,
-            state="fine"
+            time_value=phasebin,
+            state="fine" 
         )
-        # Move tensor to CPU and convert to NumPy
-        queried_volumes.append(query_pkg["vol"].cpu().numpy())  # Add time dimension
+        vol_pred = query_pkg["vol"]
+        vol_pred_list.append(vol_pred)
+
+    eval_dicts_4d = []
+    eval_dict_4d = {
+            "psnr_4d": 0.0,
+            "Average_psnr_3d": 0.0,
+            "Average_ssim_3d": 0.0,
+            "Average_ssim_3d_x": 0.0,
+            "Average_ssim_3d_y": 0.0,
+            "Average_ssim_3d_z": 0.0,
+        }
+
+    for phasebin in range(len(gt_vols)):
+        vol_pred = vol_pred_list[phasebin]
+        vol_gt = gt_vols[phasebin]
+
+        psnr_3d, _ = metric_vol(vol_gt, vol_pred, "psnr")
+        ssim_3d, ssim_3d_axis = metric_vol(vol_gt, vol_pred, "ssim")
+
+        multithread_write(
+            [vol_gt[..., i][None] for i in range(vol_gt.shape[2])],
+            slice_save_path,
+            "_gt",
+        )
+        multithread_write(
+            [vol_pred[..., i][None] for i in range(vol_pred.shape[2])],
+            slice_save_path,
+            "_pred",
+        )
+        eval_dict = {
+            "psnr_3d": psnr_3d,
+            "ssim_3d": ssim_3d,
+            "ssim_3d_x": ssim_3d_axis[0],
+            "ssim_3d_y": ssim_3d_axis[1],
+            "ssim_3d_z": ssim_3d_axis[2],
+        }
+
+        eval_dict_4d["Average_psnr_3d"] += psnr_3d / len(gt_vols)
+        eval_dict_4d["Average_ssim_3d"] += ssim_3d / len(gt_vols)
+        eval_dict_4d["Average_ssim_3d_x"] += ssim_3d_axis[0] / len(gt_vols)
+        eval_dict_4d["Average_ssim_3d_y"] += ssim_3d_axis[1] / len(gt_vols)
+        eval_dict_4d["Average_ssim_3d_z"] += ssim_3d_axis[2] / len(gt_vols)
+        eval_dicts_4d.append(eval_dict)
+
+    vol_pred = np.stack([t.cpu().numpy() for t in vol_pred_list], axis=-1)
+    vol_gt = np.stack([t.cpu().numpy() for t in gt_vols], axis=-1)
+
+    psnr_4d, _ = metric_vol(vol_gt, vol_pred, "4dpsnr")
+
+    eval_dict_4d["psnr_4d"] = float(psnr_4d)
+
+    with open(osp.join(save_path, "eval4d.yml"), "w") as f:
+        yaml.dump(eval_dict_4d, f, default_flow_style=False, sort_keys=False)
+
+    with open(osp.join(save_path, "eval4d_per_phasebin.yml"), "w") as f:
+        for phase, eval_dict in enumerate(eval_dicts_4d):
+            f.write(f"Phasebin {phase}\n")
+            yaml.dump(eval_dict, f, default_flow_style=False, sort_keys=False)
+            f.write("\n")  # Add a newline for better readability between entries
+
+    print([t.shape for t in vol_pred_list])
+    print([t.shape for t in gt_vols])
+    print(vol_gt.shape)
+    print(vol_pred.shape)
 
 
-    volumes_4d = np.stack(queried_volumes, axis=-1)  # Shape: [X, Y, Z, T]
-    nifti_img = nib.Nifti1Image(volumes_4d, affine=np.eye(4))
-    nib.save(nifti_img, niftii_savepath)
-    print(f"Saved 4D NIfTI file to {niftii_savepath}")
+    np.save(osp.join(save_path, "vol_gt.npy"), t2a(vol_gt))
+    np.save(osp.join(save_path, "vol_pred.npy"), t2a(vol_pred))
+
+    vol_gt_nifti = nib.Nifti1Image(vol_gt, affine=np.eye(4))  # Replace np.eye(4) with the correct affine if available
+    vol_pred_nifti = nib.Nifti1Image(vol_pred, affine=np.eye(4))  # Replace np.eye(4) with the correct affine if available
+
+    # Save the Nifti1Image objects
+    nib.save(vol_gt_nifti, os.path.join(save_path, "vol_gt.nii.gz"))
+    nib.save(vol_pred_nifti, os.path.join(save_path, "vol_pred.nii.gz"))
+
+    print(f"{name} complete. psnr_4d: {eval_dict_4d['psnr_4d']}, average psnr_3d: {eval_dict_4d['Average_psnr_3d']}, average ssim_3d: {eval_dict_4d['Average_ssim_3d']}")
 
 
 def evaluate_render(save_path, name, views, gaussians, pipeline, deform_net):
@@ -298,8 +290,7 @@ if __name__ == "__main__":
     parser.add_argument("--iteration", default=-1, type=int)
     parser.add_argument("--skip_render_train", action="store_true", default=False)
     parser.add_argument("--skip_render_test", action="store_true", default=False)
-    parser.add_argument("--skip_recon", action="store_true", default=True)
-    parser.add_argument("--skip_4dct", action="store_true", default=False)
+    parser.add_argument("--skip_recon", action="store_true", default=False)
     args = get_combined_args(parser)
 
     safe_state(args.quiet)
@@ -313,5 +304,4 @@ if __name__ == "__main__":
             args.skip_render_train,
             args.skip_render_test,
             args.skip_recon,
-            args.skip_4dct,
         )
